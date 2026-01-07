3a375d22ff1db134cb493ff0e4f7b7aff8f50d9f
Leela <leelanarayan1214@gmail.com>
2026-01-07 21:41:17 +0530

    Routes are Created

--- app.py
+++ app.py
@@ -1,41 +1,45 @@
 from flask import Flask,render_template,request,redirect,url_for
 app = Flask(__name__)
+accounts={}
 @app.route('/')
 def Home():
     return render_template('register.html')
 
-@app.route('/create',methods=['POST'])
-def Create():
-    acc_no = request.form.get('acc_no')
-    username = request.form.get('username')
-    balance = request.form.get('balance')
-    return redirect(url_for('Dashboard',acc_no=acc_no,username=username,balance=balance))
-@app.route('/dashboard')
-def Dashboard():
-    acc_no = request.args.get('acc_no')
-    username = request.args.get('username')
-    balance = request.args.get('balance')
-    return render_template('dashboard.html', acc_no=acc_no,
-        username=username,
-        balance=balance)
-@app.route('/withdraw',methods=['POST'])
-def Withdraw():
-    acc_no = request.form.get('acc_no')
-    username = request.form.get('username')
-    balance = float(request.form.get('balance'))
-    withdraw_amount = float(request.form.get('withdraw_amount'))
-    if withdraw_amount > balance:
-        message = "Insufficient funds!"
-        new_balance = balance
+@app.route("/create", methods=["POST"])
+def create_account():
+    acc_no = request.form["acc_no"]
+    name = request.form["name"]
+    balance = int(request.form["balance"])
+    accounts[acc_no] = {"name": name, "balance": balance}
+    return redirect(url_for("dashboard", acc_no=acc_no))
+# DASHBOARD
+@app.route("/dashboard/<acc_no>")
+def dashboard(acc_no):
+    data = accounts[acc_no]
+    return render_template("dashboard.html", acc_no=acc_no, data=data)
+# DEPOSIT
+@app.route("/deposit/<acc_no>", methods=["POST"])
+def deposit(acc_no):
+    amount = int(request.form["amount"])
+    accounts[acc_no]["balance"] += amount
+    return redirect(url_for("dashboard", acc_no=acc_no))
+
+@app.route('/withdraw/<acc_no>',methods=['POST'])
+def Withdrawn(acc_no):
+    amount = int(request.form['amount'])  # Get withdrawal amount
+
+    if accounts[acc_no]['balance'] >= amount:
+        accounts[acc_no]['balance'] -= amount  # Deduct from balance
+        return redirect(url_for("dashboard", acc_no=acc_no))
     else:
-        new_balance = balance - withdraw_amount
-        message = f"Withdrawal of {withdraw_amount} successful!"
-    return render_template('dashboard.html', acc_no=acc_no,
-        username=username,
-        balance=new_balance,
-        message=message)
-
-
+        # If insufficient balance, show message with link to go back
+        return f'Requested amount is greater than current balance <br> <a href="/dashboard/{acc_no}">Go Back to Dashboard</a>'
+    
+# DELETE ACCOUNT
+@app.route("/delete/<acc_no>")
+def delete(acc_no):
+    accounts.pop(acc_no)
+    return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True)
